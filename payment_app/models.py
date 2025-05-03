7c7bad7b9c3fee42d5cd60d59815badba9344c6a
adhulyamaya <adhulyamaya927@gmail.com>
2025-05-03 20:27:19 +0530

    payment integrations

--- payment_app/models.py
+++ payment_app/models.py
@@ -1,16 +1,56 @@
 from django.db import models
 from user_app.models import User
 
-# Create your models here.
+
+PLAN_ATTEMPTS = {
+    "free": 5, 
+    "pro": 10,   
+    "premium": 50  
+}
 class Payment(models.Model):
+    PENDING = 'pending'
+    COMPLETED = 'completed'
+    FAILED = 'failed'
+    
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (COMPLETED, 'Completed'),
+        (FAILED, 'Failed'),
+    ]
+    
+    FREE = 'free'
+    PRO = 'pro'
+    PREMIUM = 'premium'
+    
+    PLAN_CHOICES = [
+        (FREE, 'Free'),
+        (PRO, 'Pro'),
+        (PREMIUM, 'Premium'),
+    ]
+    
+    PLAN_PRICES = {
+        PRO: 499,
+        PREMIUM: 999
+    }
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payments')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, default='pending')
-    method = models.CharField(max_length=20,  default='card')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
+    method = models.CharField(max_length=20, default='card')
     transaction_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
     order_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
     payment_date = models.DateTimeField(auto_now_add=True)
+    plan = models.CharField(max_length=20, choices=PLAN_CHOICES, default=FREE)
 
     def __str__(self):
-        return f"{self.user.username} - ₹{self.amount} - {self.status}"
+        return f"{self.user.username} - ₹{self.amount} - {self.status} - {self.plan}"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.status == self.COMPLETED:
+            plan_type = self.plan
+            if plan_type in PLAN_ATTEMPTS:
+                self.user.membership_type = plan_type
+                self.user.attempt = PLAN_ATTEMPTS[plan_type]
+                self.user.save()
+
